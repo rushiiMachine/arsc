af246ef0a3d02fe8600a83287451b2bee7f36939
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-01-31 13:38:01 -0800

    chore: update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,23 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.20"
+	kotlin("jvm") version "1.9.22"
 }
 
 group = "com.github.diamondminer88"
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-	implementation(kotlin("stdlib"))
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
 	explicitApi()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+tasks.test {
+	useJUnitPlatform()
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(kotlin("stdlib"))
+	testImplementation(kotlin("test"))
 }
