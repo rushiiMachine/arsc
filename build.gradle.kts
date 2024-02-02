94ec247f10da5927b60516ef4be14ecf7994f197
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-02-02 15:47:31 -0800

    ci: add build/test workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
 	kotlin("jvm") version "1.9.22"
+	id("maven-publish")
 	id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.14.0"
 }
 
-group = "dev.materii.rushii.arsc"
 version = "1.0.0"
 
 kotlin {
@@ -31,4 +31,20 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 	testImplementation(kotlin("test"))
+}
+
+publishing {
+	repositories {
+		mavenLocal()
+	}
+
+	publications {
+		register(project.name, MavenPublication::class) {
+			artifactId = "arsc"
+			groupId = "dev.materii.rushii"
+
+			artifact(tasks["jar"])
+			artifact(tasks["kotlinSourcesJar"])
+		}
+	}
 }
